3c93550f9b2194f7055d28fbd77ca6d1394675fe
Sreyans Bothra <43511601+sreyansb@users.noreply.github.com>
2021-07-29 21:50:45 +0530

    Made compatible with new WhatsApp changes

--- birthday.py
+++ birthday.py
@@ -4,14 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-#import mysql.connector
 from datetime import datetime
 import os
 
 def sendwishes(x,event="Birthday"):
     PATH="C:\Program Files (x86)\chromedriver.exe"
     options = webdriver.ChromeOptions()
-    options.add_argument('--user-data-dir=C:/Users/sreyans/yo/User_Data')#add location to support cookies and session info
+    options.add_argument('--user-data-dir=C:/Users/sreyans/yo/User_Data')#to store cookie info
 
     driver = webdriver.Chrome(executable_path=PATH,options=options)
     driver.get('https://web.whatsapp.com/')
@@ -28,8 +27,8 @@ def sendwishes(x,event="Birthday"):
             input_box_search.send_keys(target,Keys.ENTER)
             print("Target Successfully Selected")
             time.sleep(1)
-
-            inp_xpath = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]"
+            inp_xpath = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[1]"#to find the input box for receiver
+            #inp_xpath="/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[1]/div/div[1]"
             input_box = WebDriverWait(driver,20).until(EC.presence_of_element_located((
                 By.XPATH, inp_xpath)))
             time.sleep(5)
@@ -49,13 +48,15 @@ def sendwishes(x,event="Birthday"):
         driver.quit()
 
 def fn(curday,curmonth):
-    curday=str(curday)
-    curmonth=str(curmonth)
+    #print(curday,curmonth,type(curday))
     f=open("C:/Users/sreya/Downloads/birthdays.csv","r")
     s=f.readlines()
     f.close()
     s=s[1:]
-    res=[i[-1] for i in s if (i[1]==curday and i[2]==curmonth)]
+    s=[(i[:-1].replace('"',"")).split(",") for i in s]
+    #print(s)
+    res=[",".join(i[3:]) for i in s if (int(i[1])==curday and int(i[2])==curmonth)]
+    print(res)
     flag=1
     if res:
         flag=0
@@ -68,8 +69,8 @@ def fn(curday,curmonth):
     s=f.readlines()
     f.close()
     s=s[1:]
-    
-    res=[i[-1] for i in s if (i[1]==curday and i[2]==curmonth)]
+    s=[(i[:-1].replace('"',"")).split(",") for i in s]
+    res=[",".join(i[3:]) for i in s if (int(i[1])==curday and int(i[2])==curmonth)]
     if res:
         flag=0
         sendwishes(res,"Anniversary")
@@ -79,96 +80,6 @@ def fn(curday,curmonth):
         sendwishes(["me"],"NONE")
     time.sleep(1)
 
-curday=str(datetime.now().day)
-curmonth=str(datetime.now().month)
+curday=(datetime.now().day)
+curmonth=(datetime.now().month)
 fn(curday,curmonth)
-
-'''
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-import mysql.connector
-from datetime import datetime
-import os
-
-def sendwishes(x,event="Birthday"):
-    PATH="C:\Program Files (x86)\chromedriver.exe"
-    options = webdriver.ChromeOptions()
-    options.add_argument('--user-data-dir=C:/Users/sreya/yo/User_Data')
-
-    driver = webdriver.Chrome(executable_path=PATH,options=options)
-    driver.get('https://web.whatsapp.com/')
-    try:
-        #wait for max 200s
-        #whatsapp loads but the search button does not appear and hence WebDriverWait to wait until search loads
-        initi = WebDriverWait(driver, 200).until(
-        EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[3]/div/div[1]/div/label/div/div[2]")))
-        #initi = WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.CLASS_NAME, "C28xL")))
-        for target in x:
-            print("Wishing",target,"on their",event)
-            input_box_search=driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[1]/div/label/div/div[2]')
-            input_box_search.click()
-            input_box_search.send_keys(target,Keys.ENTER)
-            print("Target Successfully Selected")
-            time.sleep(1)
-
-            inp_xpath = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]"
-            input_box = WebDriverWait(driver,20).until(EC.presence_of_element_located((
-                By.XPATH, inp_xpath)))
-            time.sleep(5)
-            string="Happy "+event+"!"
-            input_box.send_keys(string)
-            time.sleep(1)
-            input_box.send_keys(Keys.ENTER)
-            time.sleep(2)
-            print("Successfully Send Message to : "+ target + '\n')
-            print("DONE")
-    except:
-        print()
-    finally:
-        print("DONE all")
-        #whenever qr code dena padega, usko driver.quit() nahi karke
-        #khud hi quit karna hoga->manually
-        driver.quit()
-
-def fn(curday,curmonth):
-    mydb=mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="mydatabase")
-    myc=mydb.cursor()
-    sql=("SELECT name_as_per_whatsapp from birthdays where DAY=%s and MONTH=%s")
-    myc.execute(sql,(curday,curmonth))#for %s ->curday
-    res=myc.fetchall()#many people can have birthdays on same day
-    res=[x[0] for x in res]
-    k=res.copy()
-    if res:
-        sendwishes(res)
-    else:
-        print("NO birthday")
-    time.sleep(1)
-    sql=("SELECT name_as_per_whatsapp from anniversaries where DAY=%s and MONTH=%s")
-    myc.execute(sql,(curday,curmonth))
-    res=myc.fetchall()#many people can have anniversaries on same day
-    res=[x[0] for x in res]
-    k+=res
-    if res:
-        sendwishes(res,"Anniversary")
-    else:
-        print("NO anniversaries")
-    if not(k):
-        sendwishes(["me"],"NONE")
-    time.sleep(1)
-    mydb.close()
-
-curday=str(datetime.now().day)
-curmonth=str(datetime.now().month)
-os.system("start C:/xampp/xampp-control.exe")
-time.sleep(25)
-fn(curday,curmonth)
-os.system("taskkill /f /im xampp-control.exe")
-'''
